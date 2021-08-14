61ad730a2da145c2693733b597c321792bac5c52
apmelqui <apmelqui@gmail.com>
2021-08-14 08:54:51 -0400

    Layout changes and corrections

--- Assignment04-Adriano_Melquiades/MainWindow.xaml.cs
+++ Assignment04-Adriano_Melquiades/MainWindow.xaml.cs
@@ -20,41 +20,66 @@ namespace Assignment04_Adriano_Melquiades {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
             BtnCalculate.Click += CalculateBMI;
-            BtnCalculate.Content = "something elese";
+            BtnCalculate.MouseEnter += BtnMouseEnter;
+            BtnCalculate.MouseLeave += BtnMouseLeave;
 
         }
         //Event handler for Calculate button
         private void CalculateBMI(object sender, EventArgs e) {
-            string gender = txtGender.Text;
-            string age = txtAge.Text;
-            string height = txtHeight.Text;
-            string weight = txtWeight.Text;
 
-
-            if (string.IsNullOrWhiteSpace(gender) || string.IsNullOrWhiteSpace(age) || string.IsNullOrWhiteSpace(height) || string.IsNullOrWhiteSpace(weight)) {
+            if (string.IsNullOrWhiteSpace(txtGender.Text) || string.IsNullOrWhiteSpace(txtAge.Text) || string.IsNullOrWhiteSpace(txtHeight.Text) 
+                || string.IsNullOrWhiteSpace(txtWeight.Text)) {
                 MessageBox.Show("Please, input all the required info.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
+            
+            string gender = txtGender.Text;
+
+            int age;
+            if (!Int32.TryParse(txtAge.Text, out age)) {
+                MessageBox.Show("Please, input the correct age.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            //TODO: mesma coisa acima
+            double height = Convert.ToDouble(txtHeight.Text);
+            double weight =Convert.ToDouble(txtWeight.Text);
 
 
+            Create();
+        }
+
+        private void BtnMouseEnter(object sender, EventArgs e) {
+            BtnCalculate.FontSize += 5;
+        }
 
 
-
-
-
+        private void BtnMouseLeave(object sender, EventArgs e) {
+            BtnCalculate.FontSize -= 5;
         }
 
 
 
+        private void BtnToTest_Click(object sender, RoutedEventArgs e) {
+            labelTest.Content = "changed";
+            labelTest.Foreground = Brushes.Green;
+            labelTest.FontSize += 1;
+        }
 
+  
+        private void Create() {
+            //TODO
+        }
 
+        private void Read() {
 
+        }
 
-
-
-
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            
+        }
     }
 
 }
